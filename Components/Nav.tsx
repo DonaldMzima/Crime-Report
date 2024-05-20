00540d175e6fe660460071b5398dca440bd0e563
Tshidiso Donald Mzima <donaldmzima8@gmail.com>
2024-05-20 20:37:42 +0200

    ref:add Icons

--- Components/Nav.tsx
+++ Components/Nav.tsx
@@ -1,37 +1,55 @@
 import React from 'react'
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
 import { NavigationContainer } from '@react-navigation/native'
-import { Button, View, Text } from 'react-native'
 import HomePage from './HomePage'
 import ReportForm from './IncidentReportForm'
+import CameraFile from './CameraFile'
+import { View } from 'react-native'
+import Ionicons from 'react-native-vector-icons/Ionicons'
 
 const Tab = createBottomTabNavigator()
 
-const ScreenWithButton = ({ navigation }: any) => (
-  <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-    <Text>Screen with Button</Text>
-    <Button
-      title="Go to another screen"
-      onPress={() => navigation.navigate('AnotherScreen')}
-    />
-  </View>
-)
-
 const AnotherScreen = () => (
   <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-    <Text>Another Screen</Text>
+    {/* <Text>Another Screen</Text> */}
   </View>
 )
 
 const App = () => {
   return (
-    <Tab.Navigator>
-      <Tab.Screen
-        name="ScreenWithButton"
-        component={HomePage}
-        options={{ tabBarButton: () => null }} // Hide button
-      />
-      <Tab.Screen name="AnotherScreen" component={ReportForm} />
+    <Tab.Navigator
+      screenOptions={({ route }) => ({
+        tabBarIcon: ({ focused, color, size }) => {
+          let iconName
+
+          switch (route.name) {
+            case 'HomePage':
+              iconName = focused ? 'home' : 'home-outline'
+              break
+            case 'ReportForm':
+              iconName = focused ? 'document' : 'document-outline'
+              break
+            case 'TakePicture':
+              iconName = focused ? 'camera' : 'camera-outline'
+              break
+            case 'AnotherScreen':
+              iconName = focused
+                ? 'ellipsis-horizontal'
+                : 'ellipsis-horizontal-outline'
+              break
+            default:
+              iconName = 'help-circle'
+              break
+          }
+
+          return <Ionicons name={iconName} size={size} color={color} />
+        },
+      })}
+    >
+      <Tab.Screen name="HomePage" component={HomePage} />
+      <Tab.Screen name="ReportForm" component={ReportForm} />
+      <Tab.Screen name="TakePicture" component={CameraFile} />
+      <Tab.Screen name="AnotherScreen" component={AnotherScreen} />
     </Tab.Navigator>
   )
 }
